910e8e25bc21c068bb7d6b50708e908e53e5e888
ChoyonUddin <choyonuddin17@gmail.com>
2022-05-03 03:02:50 -0400

    Completed: Check For Edge Cases

--- AsterickPalindrome.py
+++ AsterickPalindrome.py
@@ -1,3 +1,4 @@
+import timeit
 #This one isn't leetCode it was Just an interview question that I couldn't get at the time cause I was nervous :(
 #Difficulty: Easy
 #Description: Basically it is the same as checking a valid palindrome. The exception is that the str contains an * for one of the 
@@ -10,11 +11,13 @@
 
 #Update: Made more efficiently using .count(*) therefore elminating edge case check
 
+
 def asterick(word : str):
-    for i in range(len(word.lower())):
-        if word.count('*')>1:
+
+    if word.count('*')>1:
             return(False)
-        
+
+    for i in range(len(word.lower())):
         if (word[i] != word[len(word)-1-i]):
             if word[i] == '*' or word[len(word)-1-i] == '*':
                 continue
@@ -22,3 +25,5 @@ def asterick(word : str):
 
     return(True)
 print(asterick('*aceca*'))
+
+
