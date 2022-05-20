1d11a23a4fca369ff9ef39de77079741fab22e88
ChoyonUddin <choyonuddin17@gmail.com>
2022-05-20 02:26:25 -0400

    Completed: #338

---
+++ CountingBits.py
@@ -0,0 +1,17 @@
+#338. Counting Bits
+#Difficulty: Esay
+
+#Description: Given an integer n, 
+#return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation
+# of i.
+
+#Progress: Completed 
+
+#Notes:
+from typing import List
+def countBits(n: int) -> List[int]:
+    ans = []
+    for i in range(n+1):
+        ans.append(bin(i).count("1"))
+    return ans
+print(countBits(2))
