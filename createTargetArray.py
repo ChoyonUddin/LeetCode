db716a2dc53f90522f228f6b78376a98ce41c705
ChoyonUddin <82260573+ChoyonUddin@users.noreply.github.com>
2023-05-06 06:23:02 -0400

    Update createTargetArray.py

--- createTargetArray.py
+++ createTargetArray.py
@@ -1,19 +1,1 @@
-'''
-Given two arrays of integers nums and index. Your task is to create target array under the following rules:
 
-Initially target array is empty.
-From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
-Repeat the previous step until there are no elements to read in nums and index.
-Return the target array.
-
-It is guaranteed that the insertion operations will be valid.
-
-Status: Completed
-Notes: simple solution probably could optimize it better, one-liner possibly
-'''
-def createTargetArray(nums,index):
-    target = []
-    for i,j in zip(nums,index):
-        target.insert(j,i)
-    return target
-print(createTargetArray([0,1,2,3,4],[0,1,2,2,1]))
