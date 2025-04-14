e856b488dcf3e3e6800c172fa50088a06745c390
ChoyonUddin <choyonuddin17@gmail.com>
2025-04-14 00:20:08 -0400

    Initial commit

---
+++ test.py
@@ -0,0 +1,8 @@
+def strStr(haystack: str, needle: str):
+
+    for i in range(len(haystack)):
+        if haystack[i:len(needle)+i]==needle:
+            print(haystack[i:i+len(needle)])
+            return i
+    return -1
+print(strStr(haystack = "aaa", needle = "aaa"))
