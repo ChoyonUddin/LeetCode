ae208b2a086df7551d474210ba6f17817f3475f1
ChoyonUddin <choyonuddin17@gmail.com>
2025-05-04 02:04:23 -0400

    May the 4th be with you

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-def strStr(haystack: str, needle: str):
+# def strStr(haystack: str, needle: str):
 
-    for i in range(len(haystack)):
-        if haystack[i:len(needle)+i]==needle:
-            print(haystack[i:i+len(needle)])
-            return i
-    return -1
-print(strStr(haystack = "aaa", needle = "aaa"))
+#     for i in range(len(haystack)):
+#         if haystack[i:len(needle)+i]==needle:
+#             print(haystack[i:i+len(needle)])
+#             return i
+#     return -1
+# print(strStr(haystack = "aaa", needle = "aaa"))
