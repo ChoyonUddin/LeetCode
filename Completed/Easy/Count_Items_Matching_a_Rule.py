d0075039152e4588086ab51adeafbb2915ff521d
ChoyonUddin <choyonuddin17@gmail.com>
2025-06-04 03:14:37 -0400

    Initial Commit

---
+++ Completed/Easy/Count_Items_Matching_a_Rule.py
@@ -0,0 +1,49 @@
+'''
+1773. Count Items Matching a Rule
+Difficulty: Easy
+You are given an array items, where each items[i] = [typei, colori, namei] describes the type, color, and name of the ith item. You are also given a rule represented by two strings, ruleKey and ruleValue.
+
+The ith item is said to match the rule if one of the following is true:
+
+ruleKey == "type" and ruleValue == typei.
+ruleKey == "color" and ruleValue == colori.
+ruleKey == "name" and ruleValue == namei.
+Return the number of items that match the given rule.
+
+ 
+
+Example 1:
+
+Input: items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], ruleKey = "color", ruleValue = "silver"
+Output: 1
+Explanation: There is only one item matching the given rule, which is ["computer","silver","lenovo"].
+Example 2:
+
+Input: items = [["phone","blue","pixel"],["computer","silver","phone"],["phone","gold","iphone"]], ruleKey = "type", ruleValue = "phone"
+Output: 2
+Explanation: There are only two items matching the given rule, which are ["phone","blue","pixel"] and ["phone","gold","iphone"]. Note that the item ["computer","silver","phone"] does not match.
+ 
+
+Constraints:
+
+1 <= items.length <= 104
+1 <= typei.length, colori.length, namei.length, ruleValue.length <= 10
+ruleKey is equal to either "type", "color", or "name".
+All strings consist only of lowercase letters.
+
+'''
+from typing import List
+def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
+    res =0
+    match ruleKey:
+        case "type":
+            pos = 0
+        case "color":
+            pos = 1
+        case "name":
+            pos = 2
+    for i in items:
+        if i[pos]==ruleValue:
+            res+=1
+    return res
+    
